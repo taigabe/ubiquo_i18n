291ae8a12f987ed826965c6200873978504e3fc4
Bernat Foj Capell <bfoj@gnuine.com>
2009-06-10 14:47:01 +0200

    Adapted test to mysql (that doesnt have DDL transactions) [#118]

--- test/unit/adapters_test.rb
+++ test/unit/adapters_test.rb
@@ -3,27 +3,30 @@ require 'mocha'
 
 class UbiquoI18n::AdaptersTest < ActiveSupport::TestCase
   
-  def test_create_versionable_table
+  def test_create_i18n_table
     definition = nil
     ActiveRecord::Base.silence{
-      ActiveRecord::Base.connection.create_table(:test, :translatable => true){|table| definition=table}
+      ActiveRecord::Base.connection.create_table(:test, :translatable => true, :force => true){|table| definition=table}
     }
+    ActiveRecord::Base.connection.drop_table(:test)
     assert_not_nil definition[:locale]
   end
   
-  def test_dont_create_versionable_table
+  def test_dont_create_i18n_table
     definition = nil
     ActiveRecord::Base.silence{
-      ActiveRecord::Base.connection.create_table(:test){|table| definition=table}
+      ActiveRecord::Base.connection.create_table(:test, :force => true){|table| definition=table}
     }
+    ActiveRecord::Base.connection.drop_table(:test)
     assert_nil definition[:locale]
   end
   
-  def test_create_content_id_on_versionable_table
+  def test_create_content_id_on_i18n_table
     definition = nil
     ActiveRecord::Base.silence{
-      ActiveRecord::Base.connection.create_table(:test, :translatable => true){|table| definition=table}
+      ActiveRecord::Base.connection.create_table(:test, :translatable => true, :force => true){|table| definition=table}
     }
+    ActiveRecord::Base.connection.drop_table(:test)
     assert_not_nil definition[:content_id]
   end
   

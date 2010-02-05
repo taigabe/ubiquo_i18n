a1156ade4073a6ce79262023bbf6674348b41d6c
Toni Reina <areina0@gmail.com>
2010-02-05 09:40:53 +0100

    Removed onchange attr in locale selector [#229 state:resolved]

--- lib/ubiquo_i18n/extensions/helpers.rb
+++ lib/ubiquo_i18n/extensions/helpers.rb
@@ -4,8 +4,8 @@ module UbiquoI18n
     # This module provides some builtin helpers to be used in views
     module Helpers
       
-      # Returns a selector of the currently active locales, ordered alphabetically, 
-      # with the current locale selected.
+      # Returns a selector of the currently active locales, ordered 
+      # alphabetically, with the current locale selected.
       #
       # Options can contain the following options:
       #   :keep_page => unless true, the page parameter is removed in the
@@ -16,23 +16,16 @@ module UbiquoI18n
         redirect_params.delete(:page) unless options[:keep_page]
         if Locale.active.size > 1
           form_tag(url_for(redirect_params), :method => :get) +
-            html_unescape(select_tag( "locale", 
-              options_from_collection_for_select(
-                Locale.active.ordered_alphabetically,
-                :iso_code,
-                :native_name,
-                current_locale
-              ),
-              :onchange => "up('form').submit();"
-              )) +
+            html_unescape(select_tag("locale_selector",
+                                     options_for_locale_selector)) +
             "</form>"
-        end
       end
       
       # For a given model, show their translations with a link to them
       def show_translations(model, options = {})
         return if model.locale?(:any)
-        render :partial => "shared/ubiquo/model_translations", :locals => {:model => model, :options => options}
+        render :partial => "shared/ubiquo/model_translations", 
+               :locals => {:model => model, :options => options}
       end
       
       # Adds a tab to display the locales section inside superadmin area
@@ -43,6 +36,13 @@ module UbiquoI18n
           tab.highlights_on({:controller => "ubiquo/locales"})
           tab.link = ubiquo_locales_path
         end if ubiquo_config_call(:assets_permit, {:context => :ubiquo_media})
+      end
+      
+      def options_for_locale_selector
+        options_from_collection_for_select(Locale.active.ordered_alphabetically,
+                                           :iso_code, 
+                                           :native_name, 
+                                           current_locale)
       end
     end
   end

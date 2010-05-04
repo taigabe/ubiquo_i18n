f99b45ffd6251f52e3c19f9f28f71330c439af84
Bernat Foj Capell <bfoj@gnuine.com>
2010-05-04 12:51:39 +0200

    Added support for the :translatable option in change_table [#154]

--- lib/ubiquo_i18n/adapters/schema_statements.rb
+++ lib/ubiquo_i18n/adapters/schema_statements.rb
@@ -2,31 +2,58 @@ module UbiquoI18n
   module Adapters
     # Extends the create_table method to support the :translatable option
     module SchemaStatements
-      
+
       # Perform the actual linking with create_table
       def self.included(klass)
         klass.send(:alias_method_chain, :create_table, :translatable)
+        klass.send(:alias_method_chain, :change_table, :translatable)
       end
-      
+
       # Parse the :translatable option as a create_table extension
       # This will currently add two fields:
       #   table.locale: string
       #   table.content_id sequence
       # with their respective indexes
-      def create_table_with_translatable(table_name, options={})
+      def create_table_with_translatable(*args, &block)
+        SchemaStatements.apply_translatable_option!(:create_table, self, *args, &block)
+      end
+
+      # Parse the :translatable option as a change_table extension
+      # This will currently add two fields:
+      #   table.locale: string
+      #   table.content_id sequence
+      # with their respective indexes
+      def change_table_with_translatable(*args, &block)
+        SchemaStatements.apply_translatable_option!(:change_table, self, *args, &block)
+      end
+
+      # Performs the actual job of applying the :translatable option
+      def self.apply_translatable_option!(method, adapter, table_name, options = {})
         translatable = options.delete(:translatable)
-        create_table_without_translatable(table_name, options) do |table_definition|
-          yield table_definition
+        method_name = "#{method}_without_translatable"
+
+        # not all methods accept the options hash
+        args = [table_name]
+        args << options if adapter.method(method_name).arity != 1
+
+        adapter.send(method_name, *args) do |table|
           if translatable
-            table_definition.string :locale, :nil => false
-            table_definition.sequence table_name, :content_id 
+            table.string :locale, :nil => false
+            table.sequence table_name, :content_id
+          end
+          yield table
+        end
+
+        # create indexes for these new fields
+        if translatable
+          [:locale, :content_id].each do |index|
+            unless adapter.indexes(table_name).map(&:columns).flatten.include? index.to_s
+              adapter.add_index table_name, index
+            end
           end
         end
-        if translatable
-          add_index table_name, :locale
-          add_index table_name, :content_id unless indexes(table_name).map(&:columns).flatten.include? "content_id"
-        end
       end
+
     end
   end
 end
--- test/unit/adapters_test.rb
+++ test/unit/adapters_test.rb
@@ -19,7 +19,7 @@ class UbiquoI18n::AdaptersTest < ActiveSupport::TestCase
     ActiveRecord::Base.connection.drop_table(:test)
     assert_nil definition[:locale]
   end
-  
+
   def test_create_content_id_on_i18n_table
     definition = nil
     ActiveRecord::Base.silence{
@@ -28,5 +28,31 @@ class UbiquoI18n::AdaptersTest < ActiveSupport::TestCase
     ActiveRecord::Base.connection.drop_table(:test)
     assert_not_nil definition[:content_id]
   end
+
+  def test_change_table_with_translatable
+    connection = ActiveRecord::Base.connection
+    ActiveRecord::Base.silence{
+      connection.create_table(:test, :force => true){}
+      connection.change_table(:test, :translatable => true){}
+    }
+    column_names = connection.columns(:test).map(&:name).map(&:to_s)
+    assert column_names.include?('content_id')
+    assert column_names.include?('locale')
+    assert_equal 1, connection.list_sequences("test_$").size
+    connection.drop_table(:test)
+  end
   
+  def test_change_table_without_translatable
+    connection = ActiveRecord::Base.connection
+    ActiveRecord::Base.silence{
+      connection.create_table(:test, :force => true){}
+      connection.change_table(:test){}
+    }
+    column_names = connection.columns(:test).map(&:name).map(&:to_s)
+    assert !column_names.include?('content_id')
+    assert !column_names.include?('locale')
+    assert_equal 0, connection.list_sequences("test_$").size
+    connection.drop_table(:test)
+  end
+
 end

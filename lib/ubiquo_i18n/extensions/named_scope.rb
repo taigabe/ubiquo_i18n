49c17a469fbb5c747f012b0f0686100fa5dda455
Bernat Foj Capell <bfoj@gnuine.com>
2010-12-14 18:26:48 +0100

    Refactoring to avoid preloading of proxy_found in locale named scope when used after other scopes [#310 state:resolved]

--- lib/ubiquo_i18n/extensions/named_scope.rb
+++ lib/ubiquo_i18n/extensions/named_scope.rb
@@ -14,20 +14,29 @@ module UbiquoI18n
           # this is a locale() call
           # find the model we are acting on
           klass = proxy_scope
-          while !klass.is_a?(Class)
+          while !(Class === klass)
             if klass.respond_to?(:proxy_scope)
               klass = klass.proxy_scope
             elsif klass.is_a?(Array)
               klass = klass.first.class
             end
           end
-          # tell the model that we are heading there
+          # tell the (adequate) model that we are heading there
           if klass.respond_to?(:really_translatable_class)
-            good_klass = klass.really_translatable_class
-            good_klass.instance_variable_set('@locale_scoped', true)
-            current_locale_list = good_klass.instance_variable_get('@current_locale_list')
-            (current_locale_list ||= []) << proxy_options[:locale_list]
-            good_klass.instance_variable_set('@current_locale_list', current_locale_list)
+            klass = klass.really_translatable_class
+            unless klass.instance_variable_get(:@setting_locale_list)
+              klass.instance_variable_set(:@locale_scoped, true)
+              current_locale_list = klass.instance_variable_get(:@current_locale_list)
+              (current_locale_list ||= []) << proxy_options[:locale_list]
+              klass.instance_variable_set(:@current_locale_list, current_locale_list)
+            end
+            begin
+              # avoid coming here a second time to reset the current_locale_list
+              klass.instance_variable_set(:@setting_locale_list, true)
+              return method_missing_without_locale_scope(method, *args, &block)
+            ensure
+              klass.instance_variable_set(:@setting_locale_list, false)
+            end
           end
         end
         method_missing_without_locale_scope(method, *args, &block)

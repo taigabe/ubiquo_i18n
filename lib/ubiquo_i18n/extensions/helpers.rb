c584ac43a861b588b7a9178f82277e7867068e1c
David Lozano <dlozano@gnuine.com>
2011-11-30 16:16:03 +0100

    The translations table in forms is displayed only when the object is already exists in database [#579]

--- lib/ubiquo_i18n/extensions/helpers.rb
+++ lib/ubiquo_i18n/extensions/helpers.rb
@@ -1,10 +1,10 @@
 module UbiquoI18n
   module Extensions
-    
+
     # This module provides some builtin helpers to be used in views
     module Helpers
-      
-      # Returns a selector of the currently active locales, ordered 
+
+      # Returns a selector of the currently active locales, ordered
       # alphabetically, with the current locale selected.
       #
       # Options can contain the following options:
@@ -22,14 +22,14 @@ module UbiquoI18n
             "</form>"
         end
       end
-      
+
       # For a given model, show their translations with a link to them
       def show_translations(model, options = {})
-        return if model.in_locale?(:any)
-        render :partial => "shared/ubiquo/model_translations", 
+        return if model.new_record? || model.in_locale?(:any)
+        render :partial => "shared/ubiquo/model_translations",
                :locals => {:model => model, :options => options}
       end
-      
+
       # Adds a tab to display the locales section inside superadmin area
       def superadmin_locales_tab(navtab)
         navtab.add_tab do |tab|
@@ -39,10 +39,10 @@ module UbiquoI18n
           tab.link = ubiquo_locales_path
         end if ubiquo_config_call(:assets_permit, {:context => :ubiquo_media})
       end
-      
+
       def options_for_locale_selector
         options_from_collection_for_select(Locale.active.ordered_alphabetically,
-                                           :iso_code, 
+                                           :iso_code,
                                            :humanized_name,
                                            current_locale)
       end
--- test/unit/helpers_test.rb
+++ test/unit/helpers_test.rb
@@ -19,10 +19,59 @@ class UbiquoI18n::Extensions::HelpersTest < ActionView::TestCase
     assert_select html_content.root, 'form[action=?]', /page.+/
   end
 
+  def test_show_translations_for_a_existing_object
+    model = create_model(:locale => 'test')
+    self.expects(:render).once.with(
+      :partial =>  "shared/ubiquo/model_translations",
+      :locals => { :model => model, :options => {} }
+    ).returns(true)
+
+    assert show_translations(model)
+  end
+
+  def test_show_translations_should_not_render_anything_for_not_persistent_objects
+    model = TestModel.new(:locale => 'test')
+    self.expects(:render).never.with(
+      :partial =>  "shared/ubiquo/model_translations",
+      :locals => { :model => model, :options => {} }
+    )
+
+    assert !show_translations(model)
+
+    existing_model = create_model(:locale => 'test')
+    model = existing_model.translate('test2')
+    self.expects(:render).never.with(
+      :partial =>  "shared/ubiquo/model_translations",
+      :locals => { :model => model, :options => {} }
+    )
+
+    assert !show_translations(model)
+  end
+
+  def test_show_translations_should_not_render_anything_for_object_with_locale_any
+    model = create_model(:locale => 'any')
+     self.expects(:render).never.with(
+      :partial =>  "shared/ubiquo/model_translations",
+      :locals => { :model => model, :options => {} }
+    )
+
+    assert !show_translations(model)
+  end
+
+  def test_show_translations_should_pass_options_to_the_view
+    model = create_model(:locale => 'any')
+     self.expects(:render).never.with(
+      :partial =>  "shared/ubiquo/model_translations",
+      :locals => { :model => model, :options => { :my_options => true } }
+    )
+
+    assert !show_translations(model, :my_option => true)
+  end
+
   # Some stubs for helpers
   UbiquoI18n::Extensions::Helpers.module_eval do
     include Ubiquo::Helpers::CorePublicHelpers
-    
+
     def params
       {:page => '1'}
     end
